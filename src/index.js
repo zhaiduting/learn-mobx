f9408f1721f4209098fb873b921ba61379b50cb7
zhaiduting <zhaiduting@icloud.com>
2023-10-01 15:45:04 +0800

    the-gist-of-mobx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React, {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './styles.css';
 
-import App from "./App";
+import App from './App';
+import {store, TodoListView} from './the-gist-of-mobx';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <App/>
+      <hr/>
+      <TodoListView todoList={store}/>
+    </StrictMode>,
 );
